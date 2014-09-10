69d35364472c52f3fa52c7efe5d9011bd4c3bebd
mpr0xy <mpr0xy@live.com>
2014-09-10 22:28:42 +0800

    fix dom set style

--- smart_url_copy.js
+++ smart_url_copy.js
@@ -8,7 +8,10 @@ smart_url_copy.prototype.run = function(){
   var body = document.getElementsByTagName('body')[0];
   var alertDiv = document.createElement("div");
   alertDiv.innerHTML = "点击我复制";
-  alertDiv.style = "position:fixed;top:10px;right:50px;z-index:9999;outline:#00ff00 solid"
+  alertDiv.style.top = "10px";
+  alertDiv.style.right = "50px";
+  alertDiv.style.zIndex = "9999";
+  alertDiv.style.outline = "#00ff00 solid";
   body.appendChild(alertDiv);
   var client = new ZeroClipboard(alertDiv);
   client.setText(this.smart_url);
